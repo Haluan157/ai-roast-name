eec82c252c42b1be17520d2734851d4b51fcbb24
Haluan157 <wordcarikata337@gmail.com>
2025-03-15 14:00:57 +0700

    Change

--- api/index.js
+++ api/index.js
@@ -1,13 +1,19 @@
 const express = require("express")
 const app = express()
+const path = require("path")
 const minifyHTML = require("express-minify-html")
 const { GoogleGenerativeAI } = require("@google/generative-ai");
+const DOMPurify = require("dompurify")
+const marked = require("marked")
+const { JSDOM } = require("jsdom")
+const wind = new JSDOM("").window
+const puri = DOMPurify(wind)
 
 const genAI = new GoogleGenerativeAI(process.env.API_AI);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash", systemInstruction: process.env.SYSTEM_INST});
 
 app.set("view engine", "ejs")
-app.set("views", "./views")
+app.set("views", path.join(__dirname, "views"))
 app.use(express.urlencoded({ extended: true }))
 app.use(minifyHTML({
     override: true,
@@ -31,16 +37,21 @@ app.get("/", (req, res) => {
 
 app.post("/", async (req, res) => {
   const { name } = req.body
-  let response
+  let response = name
   try {
     const result = await model.generateContent(name);
     response = result.response.text();
   } catch {
     response = "Token habis 😅😅"
   }
+  
+  const htm = marked.parse(response)
+  
   res.render("index", {
-    response
+    response: puri.sanitize(htm)
   })
 })
 
-app.listen(3000)
+app.listen(process.env.PORT || 3000)
+
+module.exports = app
